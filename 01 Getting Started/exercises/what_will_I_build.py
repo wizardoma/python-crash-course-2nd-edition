bfe593faa27e5714b09cdcc6bf22b9d6ea92a5e1
Ibekason Alexander <alibekason@gmail.com>
2023-06-03 11:58:58 +0100

    Add chapter on getting Started

---
+++ 01 Getting Started/exercises/what_will_I_build.py
@@ -0,0 +1,11 @@
+# My main objective for learning python is actually towards the side of Machine Learning and Artificial Intelligence
+# But I can still give fun projects motivating me to learn python
+
+print('''
+1. A machine learning empowered mobile app for trading stocks and crypto,
+
+
+2. A data science focused software service company to help companies understand business intelligence from their data
+
+3. An app trained on machine learning models that knows a lot about a particular concept or idea
+''')
